f012b6e4c31a72c0bdad78527b40407af039345f
jeremy <jeremywib7@gmail.com>
2022-09-06 16:57:42 +0700

    added README.md

--- src/main/java/com/fang/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
+++ src/main/java/com/fang/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -1,2 +1,16 @@
-package com.fang.rest.webservices.restfulwebservices.helloworld;public class HelloWorldBean {
+package com.fang.rest.webservices.restfulwebservices.helloworld;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class HelloWorldBean {
+
+    private String message;
+
 }
