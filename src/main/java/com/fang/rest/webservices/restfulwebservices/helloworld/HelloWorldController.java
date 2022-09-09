267ccd710d5606924a6d2ac54956b8aabe3203e0
jeremy <jeremywib7@gmail.com>
2022-09-09 17:00:08 +0700

    add international

--- src/main/java/com/fang/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/fang/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,14 +1,19 @@
 package com.fang.rest.webservices.restfulwebservices.helloworld;
 
-import com.fang.rest.webservices.restfulwebservices.user.User;
 import lombok.AllArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
+
+    private final MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -23,5 +28,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello world, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
