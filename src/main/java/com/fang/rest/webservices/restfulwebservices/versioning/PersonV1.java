c575f09c93a2f1bac902405320c1f3c3493d867d
jeremy <jeremywib7@gmail.com>
2022-09-10 18:01:03 +0700

    international x-api-version

---
+++ src/main/java/com/fang/rest/webservices/restfulwebservices/versioning/PersonV1.java
@@ -0,0 +1,11 @@
+package com.fang.rest.webservices.restfulwebservices.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PersonV1 {
+
+    private String name;
+}
