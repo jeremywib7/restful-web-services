322d969d42446aa669d866456df63c0a7208d4e0
jeremy <jeremywib7@gmail.com>
2022-09-13 17:00:00 +0700

    try actuator

--- src/main/java/com/fang/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ src/main/java/com/fang/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,12 +1,19 @@
 package com.fang.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@JsonFilter("SomeBeanFilter")
+//@JsonIgnoreProperties({"field1", "field2"})
 public class SomeBean {
     private String field1;
+
+    //    @JsonIgnore
     private String field2;
+
     private String field3;
 }
